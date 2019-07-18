7e53729524c4889230baf96412f7f94f97b94b60
Vuk Djukic <vuk.djukic_external.k2@coinbase.com>
2019-07-17 21:30:37 -0700

    adding reducers

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ProjectSummary from './ProjectSummary'
+import { Link } from 'react-router-dom'
 
 const ProjectList = ({project}) => {
     return (
         <div className = "project-list section">
             { project && project.map(project => {
                 return (
-                    <ProjectSummary project={project} key={project.id} />
+                    <Link to={'/project/' + project.id}>
+                        <ProjectSummary project={project} key={project.id} />
+                    </Link>
                 )
             })}
         </div>
