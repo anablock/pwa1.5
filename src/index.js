e0c6d6ed16ccc34b61ba70b38ccf6eb5b0c04ca6
Vuk Djukic <vuk.djukic_external.k2@coinbase.com>
2019-07-19 08:47:42 -0700

    route blocking for authentication state

--- src/index.js
+++ src/index.js
@@ -10,16 +10,24 @@ import thunk from 'redux-thunk';
 import { getFirestore } from 'redux-firestore'
 import { getFirebase } from 'react-redux-firebase'
 
-const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })));
+const store = createStore(rootReducer, 
+    compose(
+        applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+        reduxFirestore(fbConfig),
+        reactReduxFirebase(fbConfig, { attachAuthIsReady: true })
+    )
+);
 
-ReactDOM.render(
+store.firebaseAuthIsReady.then(() => {
+    ReactDOM.render(
         <Provider store={store}>
             <App />
         </Provider>,
     document.getElementById('root')
 )
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://bit.ly/CRA-PWA
+    serviceWorker.unregister();
+})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
